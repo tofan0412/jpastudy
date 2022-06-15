7f091679214e486f04a5fff8c168890255fa0e55
Admin <admin@cpe-172-90-11-43.socal.res.rr.com>
2022-06-15 15:42:12 +0900

    220615 jpa 요구사항 분석과 기본매핑 학습

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,25 +1,18 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
-// 만약 테이블 명과 클래스 명이 다르다면
-// @Table(name = "USER") 와 같이 추가한다.
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    @Column(name = "name", nullable = false) // DB에 들어갈 컬럼명은 name이다.
+    @Column(name = "name", nullable = false)
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING) // 객체에서 enum타입을 쓰려고 하는 경우, DB에는 이넘 타입이 없다. 이 경우 @Enumerated라는 걸 쓰면 된다.
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP) // DB의 경우 날짜, 시간, 날짜+시간을 구분해서 사용하기 때문에 애노테이션으로 명시
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob // VARCHAR가 허용하는 데이터 크기를 초과할 거 같은 경우에 쓴다.
-    private String description;
 
     public Member() {
     }
@@ -38,45 +31,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,15 +1,14 @@
-package hellojpa;
+package jpabook.jpashop;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         // 설정 파일 이름 작성하고 (persistenceUnitName) 팩토리 객체 생성하기
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpashop");
 
         // 쿼리를 날리려면 어떻게 해야할까?
         EntityManager em = emf.createEntityManager();
@@ -18,20 +17,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("A");
-
-            Member member2 = new Member();
-            member2.setUsername("B");
-
-            Member member3 = new Member();
-            member3.setUsername("C");
-
-            em.persist(member1);
-            em.persist(member2);
-            em.persist(member3);
-
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
